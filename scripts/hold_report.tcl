726a044b3e24f0867e3e719cd4ed6007c9faa865
NgocHiep-pdsta <phamngochiep2100@gmail.com>
2026-03-19 15:47:31 +0700

    Update hold_report.tcl

--- scripts/hold_report.tcl
+++ scripts/hold_report.tcl
@@ -2,11 +2,12 @@
 proc hold_report {master_data} {
   set hold_data [dict get $master_data hold_data]
   lassign $hold_data total violated vlist
-  set text "TOTAL: $total | VIOLATED: $violated"
-  append text "\n"
+  set text "===== HOLD TIMING REPORT =====\n"
+  append text "  TOTAL: $total | VIOLATED: $violated \n"
+  append text "---------------------------------\n"
   foreach v [lsort -real -index 1 $vlist] { 
     lassign $v cell sf
-    append text [format " %-12s  slack=%-8.3f  %s\n" $cell $sf [classify_hold $sf]]
+    append text [format "%-10s  slack=%-8.3f  %s\n" $cell $sf [classify_hold $sf]]
   }
   return $text
 }
